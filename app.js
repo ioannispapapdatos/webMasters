4915075c067144de7bcfb02f05320114ec8449a1
Jannis Paps <jannispaps@Janniss-MBP.home>
2022-01-25 10:26:16 +0200

    newuload

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
+const mode = document.querySelector(".darkmode");
 
-const mode = document.querySelector('.darkmode')
+mode.addEventListener("click", function () {
+  mode.classList.toggle("dark-mode");
+});
 
+// tidio chat code
 
-mode.addEventListener('click', function () {
-    mode.classList.toggle('dark-mode')
-})
+document.tidioChatLang = document.querySelector("html").getAttribute("lang");
+
+document.tidioChatLang = "es";
+setTimeout(function () {
+  var tidioScript = document.createElement("script");
+  tidioScript.src = "//code.tidio.co/PUBLIC_KEY.js";
+  document.body.appendChild(tidioScript);
+}, 5 * 1000);
+
+(function () {
+  function onTidioChatApiReady() {
+    window.tidioChatApi.open();
+  }
+  if (window.tidioChatApi) {
+    window.tidioChatApi.on("ready", onTidioChatApiReady);
+  } else {
+    document.addEventListener("tidioChat-ready", onTidioChatApiReady);
+  }
+})();
